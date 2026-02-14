1223cb5e39674665e0e31d7977634c78969c990a
Max Busato <maxwell.busato@student.adelaide.edu.au>
2026-02-14 15:24:05 +1030

    Updating Resource Files

--- src/resources/resource.h
+++ src/resources/resource.h
@@ -1,16 +1,46 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Resource.rc
+// ------------------------------------------------------------------------------
+// -               V E R S I O N   I D E N T I F I C A T I O N -
+// ------------------------------------------------------------------------------
+// File:                    %name: resource.h %
+// Date Created:            %date_created: Sat Feb 14 15:45:55 2026 %
+// CM Version               %version: 2 %
+// CM Instance              %instance: 1 %
+// Author:                  %created_by: MBusato %
+// ------------------------------------------------------------------------------
+// -                          D E S C R I P T I O N -
+// ------------------------------------------------------------------------------
+// Resource header file for Dwindling Galaxies Windows build. Defines resource
+// identifiers used in Resource.rc.
 //
-#define IDI_ICON1 101
+// ------------------------------------------------------------------------------
+// -                          L I C E N S I N G -
+// ------------------------------------------------------------------------------
+// Copyright (c) 2024 EastSide Lion Studios. All rights reserved. Use of this
+// software is subject to the license terms found in the LICENSE file located in
+// the root directory of this repository.
+// ------------------------------------------------------------------------------
 
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE 102
-#define _APS_NEXT_COMMAND_VALUE 40001
-#define _APS_NEXT_CONTROL_VALUE 1001
-#define _APS_NEXT_SYMED_VALUE 101
-#endif
-#endif
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+  // Unique Integer ID Assigned to the Application's Icon Resource.
+  #define IDI_ICON1 101
+
+  // Special Block Used by the Visual Studio Resource Editor.
+  #ifdef APSTUDIO_INVOKED
+
+    // Only Execute if the Editor isn't in "read-only" Mode.
+    #ifndef APSTUDIO_READONLY_SYMBOLS
+
+      // These Macros Help the IDE Keep Track of the Next Available IDs to Assign to
+      // New Resources.
+      #define _APS_NEXT_RESOURCE_VALUE 102
+      #define _APS_NEXT_COMMAND_VALUE 40001
+      #define _APS_NEXT_CONTROL_VALUE 1001
+      #define _APS_NEXT_SYMED_VALUE 101
+      
+    #endif  // APSTUDIO_READONLY_SYMBOLS
+
+  #endif  // APSTUDIO_INVOKED
+
+#endif  // RESOURCE_H
